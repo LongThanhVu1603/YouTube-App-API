bc13a6ddde63305f5674968682c765e0b05179e2
Long Thanh Vu <Longthanhvu1603hn2@gmail.com>
2021-10-22 11:00:51 +0700

    responsive

--- app2.js
+++ app2.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
     // thêm video vào html (id=#video)
     function mainVid(id) {
         $('#video').html(`
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					<iframe src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 				`);
     }
 
